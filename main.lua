bb8f485f4b8b25ddbdc4ef5c3872f5ef58a46bd1
Roger Zanoni <rogerzanoni@gmail.com>
2017-09-17 12:27:15 -0300

    Draw a test map using free assets from itch.io

--- main.lua
+++ main.lua
@@ -1,3 +1,18 @@
+local sti = require "libs/Simple-Tiled-Implementation/sti"
+local map, width, height
+
+function love.load()
+   map = sti("assets/maps/green_valley.lua")
+   width = map.width * map.tilewidth
+   height = map.height * map.tileheight
+   print("Map width:", width)
+   print("Map height:", width)
+end
+
+function love.update(dt)
+   map:update(dt)
+end
+
 function love.draw()
-   love.graphics.print("Hello World", 400, 300)
+   map:draw(-width / 2, -height / 2)
 end
