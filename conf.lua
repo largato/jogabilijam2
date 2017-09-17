8328201080c373e561c7de83678a930b9853ad18
Roger Zanoni <rogerzanoni@gmail.com>
2017-09-17 00:57:17 -0300

    Add base game and configuration

---
+++ conf.lua
@@ -0,0 +1,15 @@
+function love.conf(t)
+  t.releases = {
+    title = "jogabilijam2",
+    package = "jogabilijam2",
+    loveVersion = nil,
+    version = "0.1",
+    author = "Luiz Cavalcanti, Roger Zanoni",
+    email = "cavalcanti.luiz@gmail.com, rogerzanoni@gmail.com",
+    description = "Jogabilijam2 entry",
+    homepage = "https://github.com/largato",
+    identifier = "com.apple.package",
+    excludeFileList = {},
+    releaseDirectory = nil,
+  }
+end
