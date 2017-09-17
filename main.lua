03fa9bc1db01f5cb4ca1e606be4c0d6df2cfda09
Roger Zanoni <rogerzanoni@gmail.com>
2017-09-17 14:35:15 -0300

    Start using classic and sodapop modules
    Also create a sample player class and a snippet showing how to get
    properties from objects defined in Object Layers using tiled map editor

--- main.lua
+++ main.lua
@@ -1,8 +1,9 @@
 local sti = require "libs/Simple-Tiled-Implementation/sti"
+local sodapop = require "libs/sodapop/sodapop"
 local Camera = require "camera"
+local SampleChar = require "samplechar"
 
-local map, width, height
-
+local map, width, heigh, playerSprite, char
 
 function love.load()
    camera = Camera:new()
@@ -11,17 +12,29 @@ function love.load()
    width = map.width * map.tilewidth
    height = map.height * map.tileheight
    print("Map width:", width)
-   print("Map height:", width)
+   print("Map height:", height)
+
+   local player
+   for k, object in pairs(map.objects) do
+      if object.name == "Player" then
+         player = object
+         break
+      end
+   end
+
+   char = SampleChar(player.x, player.y, 96)
 end
 
 function love.update(dt)
    camera:update(dt)
    updateInput(dt)
    map:update(dt)
+   char:update(dt)
 end
 
 function love.draw()
    map:draw(-camera.x, -camera.y)
+   char:draw(-camera.x, -camera.y)
 end
 
 function setupDisplay()
