427c5e9a08e61e6c511fc063dd67bcd438d47e16
Luiz Carlos Cavalcanti <cavalcanti.luiz@gmail.com>
2017-09-17 22:59:19 -0300

    scene basics
    Signed-off-by: Luiz Carlos Cavalcanti <cavalcanti.luiz@gmail.com>

---
+++ scene.lua
@@ -0,0 +1,34 @@
+Scene = {
+   map = nil,
+   camera = nil
+}
+
+function Scene:new(o)
+   o = o or {}
+   setmetatable(o, self)
+   self.__index = self
+   return o
+end
+
+function Scene:setCamera(c)
+   self.camera = c
+end
+
+function Scene:setMap(m)
+   self.map = m
+end
+
+function Scene:draw()
+   local c = self.camera
+   c:set()
+   self.map:resize(c.width, c.height)
+   self.map:draw(-c.x, -c.y, c.scaleX, c.scaleY)
+   c:unset()
+end
+
+function Scene:update(dt)
+   self.camera:update(dt)
+   self.map:update(dt)
+end
+
+return Scene
