4eeceeada00485970f7da01a7c72370753910090
Luiz Carlos Cavalcanti <cavalcanti.luiz@gmail.com>
2017-09-19 22:42:21 -0300

    Add chars to manager according to map object names
    Signed-off-by: Luiz Carlos Cavalcanti <cavalcanti.luiz@gmail.com>

--- stringhelper.lua
+++ stringhelper.lua
@@ -1,3 +1,10 @@
-function string.starts(str, start)
-   return string.sub(str, 1, string.len(start)) == start
+function string:starts(start)
+   return string.sub(self, 1, string.len(start)) == start
+end
+
+function string:split(sep)
+   local sep, fields = sep or ":", {}
+   local pattern = string.format("([^%s]+)", sep)
+   self:gsub(pattern, function(c) fields[#fields+1] = c end)
+   return fields
 end
