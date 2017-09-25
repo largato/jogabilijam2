45c74df90661a34c1b5276990f447589bd0d76f9
Roger Zanoni <rogerzanoni@gmail.com>
2017-09-25 18:45:50 -0300

    Add default settings and config file handling

---
+++ assets/config/defaultsettings.lua
@@ -0,0 +1,8 @@
+return {
+   {"FullScreen", "boolean", true},
+   {"Resolução", "string", "1280x720", {"1920x1080",
+                                      "1280x720",
+                                      "1024x768",
+                                      "800x600",
+                                      "640x480"}}
+}
--- main.lua
+++ main.lua
@@ -1,6 +1,7 @@
 require "entitymanager"
 require "stringhelper"
 require "scenemanager"
+require "settings"
 
 local Map = require "libs/Simple-Tiled-Implementation/sti"
 local Camera = require "camera"
@@ -8,20 +9,20 @@ local Scene = require "scene"
 local EndScene = require "endscene"
 local MenuScene = require "menuscene"
 local DialogScene = require 'dialogscene'
+local SettingsScene = require "settingsscene"
 
 local debugMode = true
 local width = 1280
 local height = 720
 
 function love.load()
-   love.window.setMode(width, height)
-
    local map = Map("assets/maps/green_valley.lua")
    sceneManager:add("menu", MenuScene())
    sceneManager:add("intro", DialogScene('intro', "battle"))
    sceneManager:add("battle", Scene(Camera(width, height), map))
    sceneManager:add("PlayerWon", EndScene("Jogador"))
    sceneManager:add("EnemyWon", EndScene("Inimigo"))
+   sceneManager:add("settings", SettingsScene())
    sceneManager:setCurrent("menu")
 end
 
